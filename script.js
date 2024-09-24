3e75826ba1dab5aee77ec5343ae8334daa773a8c
Ikadev95 <fegughi@gmail.com>
2024-09-24 18:03:56 +0200

    commit3

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
-const resetBtn = document.getElementById("reset");
+const resetBtn = document.getElementById("remove");
 const submitBtn = document.getElementById("save");
 const form = document.getElementById("form1");
 const input = document.getElementById("name");
+const result = document.getElementById("result");
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
     localStorage.setItem('name', input.value);
-    
+    result.innerText = input.value;
+    input.value = "";
 });
 
 resetBtn.addEventListener("click", function(){
-    localStorage.clear(); 
+    localStorage.removeItem('name');
+    input.value = ""
 });
